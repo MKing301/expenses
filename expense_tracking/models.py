9f9c5dc7870e8d5419862ed2ce27df0861ab56fe
MKING301 <33675411+MKing301@users.noreply.github.com>
2022-02-04 17:03:41 -0500

    Add expense and expense type models

--- expense_tracking/models.py
+++ expense_tracking/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class ExpenseType(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Expense(models.Model):
+    expense_date = models.DateField()
+    expense_type = models.ForeignKey(
+        ExpenseType,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=200)
+    org = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=7, decimal_places=2)
+    notes = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name + '_' + self.expense_date.strftime('%m_%d_%Y')
