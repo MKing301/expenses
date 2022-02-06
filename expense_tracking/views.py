4ced9baeb78e7d87779af68390e5b32e3230373f
MKING301 <33675411+MKing301@users.noreply.github.com>
2022-02-06 13:59:54 -0500

    Update expense view with expense query set

--- expense_tracking/views.py
+++ expense_tracking/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Expense
 
 
 def expenses(request):
+    my_expenses = Expense.objects.order_by('-expense_date')
     return render(request=request,
-                  template_name="expense_tracking/expense.html"
+                  template_name="expense_tracking/expense.html",
+                  context={
+                      'my_expenses': my_expenses
+                  }
                   )
