0d381fba5b46f97692a17d0b0ca7bffe50696ae8
MKING301 <33675411+MKing301@users.noreply.github.com>
2022-02-10 20:24:46 -0500

    Add urlpattern for delete expense

--- expense_tracking/urls.py
+++ expense_tracking/urls.py
@@ -7,4 +7,9 @@ app_name = "expense_tracking"
 urlpatterns = [
     path('', views.expenses, name="expenses"),
     path('add/', views.add_expense, name="add_expense"),
+    path(
+        "delete_expense/<int:id>",
+        views.delete_expense,
+        name='delete_expense'
+    ),
 ]
