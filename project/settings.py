40278f0e4a8e14ef9dc7b62c8464b91258156baf
MKING301 <33675411+MKing301@users.noreply.github.com>
2022-02-10 22:11:31 -0500

    Add message tags to settings file

--- project/settings.py
+++ project/settings.py
@@ -14,6 +14,7 @@ import os
 
 from dotenv import load_dotenv
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 load_dotenv()
 
@@ -136,3 +137,11 @@ STATIC_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger'
+}
