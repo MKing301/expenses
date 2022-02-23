c9db3bfcab2f97eb96c2a2e80cf560ab059ddd6c
MKING301 <33675411+MKing301@users.noreply.github.com>
2022-02-22 19:14:25 -0500

    Add comments to views and models

--- expense_tracking/models.py
+++ expense_tracking/models.py
@@ -2,13 +2,19 @@ from django.db import models
 
 
 class ExpenseType(models.Model):
+    # Class for expense type
+
     name = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
+        # String method returns expense type name
+
         return self.name
 
 
 class Expense(models.Model):
+    # Class for expense
+
     expense_date = models.DateField()
     expense_type = models.ForeignKey(
         ExpenseType,
@@ -20,4 +26,6 @@ class Expense(models.Model):
     notes = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
+        # String method returns expense name and date
+
         return self.name + '_' + self.expense_date.strftime('%m_%d_%Y')
--- expense_tracking/views.py
+++ expense_tracking/views.py
@@ -11,6 +11,8 @@ from django.core.paginator import Paginator
 
 
 def index(request):
+    # Function renders landing page
+
     return render(
         request=request,
         template_name="expense_tracking/index.html",
@@ -18,6 +20,7 @@ def index(request):
 
 
 def login_request(request):
+    # Function for handling login request
 
     # Verify the user is not logged into the application
     if not request.user.is_authenticated:
@@ -89,11 +92,16 @@ def login_request(request):
             '''You are already logged in.  You must log out to log in as
             another user.'''
         )
+
+        # Redirect user to table of expense
         return redirect("expense_tracking:expenses")
 
 
 @login_required()
 def expenses(request):
+    # Function requires user to be logged in and renders a table of expenses
+    # showing the date, type organization, amount and notes (with edit and
+    # delete buttons)
 
     # Set up pagination
     # Get 20 expenses per page, by expense date descending
@@ -103,6 +111,8 @@ def expenses(request):
     page = request.GET.get('page')
     my_expenses = p.get_page(page)
 
+    # Render expense table list 20 expense per page with page navigation at
+    # the bottom of the page
     return render(request=request,
                   template_name="expense_tracking/expense.html",
                   context={
@@ -113,20 +123,36 @@ def expenses(request):
 
 @login_required()
 def add_expense(request):
+    # Function for adding an expense.  User must be logged in to access.
 
+    # Obtain expense records ordered by expense name
     expense_types = ExpenseType.objects.order_by('name')
 
+    # When the form method is post
     if request.method == "POST":
+
         form = ExpenseForm(request.POST)
+
+        # Check if form is valid
         if form.is_valid():
+
+            # If form is valid, save values to database
             form.save()
+
+            # Display successful alert message
             messages.success(
                 request,
                 'Expense added successfully.'
             )
+
+            # Redirect user to table of expense
             return redirect('expense_tracking:expenses')
 
+        # When form is invalid
         else:
+
+            # Render the form, including drop down values for expense types
+            # and display an form field errors at the top of the page in red
             return render(
                 request=request,
                 template_name="expense_tracking/add_expense.html",
@@ -135,9 +161,13 @@ def add_expense(request):
                     'expense_types': expense_types
                 }
             )
+
+    # When the form method is get
     else:
+        # Generate expense form
         form = ExpenseForm()
 
+        # Render the form, including drop down values for expense types
         return render(
             request=request,
             template_name="expense_tracking/add_expense.html",
@@ -150,6 +180,7 @@ def add_expense(request):
 
 @login_required
 def edit_expense(request, id):
+    # Function for adding an expense. User must be logged in to access.
 
     # Obtain expense record to edit by id
     expense_to_edit = Expense.objects.get(id=id)
@@ -160,19 +191,33 @@ def edit_expense(request, id):
         id=expense_to_edit.expense_type.pk
     ).order_by('name')
 
+    # When the form method is post
     if request.method == "POST":
         form = ExpenseForm(request.POST, instance=expense_to_edit)
 
+        # Check if form is valid
         if form.is_valid():
+
+            # If form is valid, save changes to database
             form.save()
+
+            # Display successful alert message
             messages.success(
                 request,
                 'Your expense was updated successfully!'
             )
+
+        # Redirect user to table of expense
         return redirect('expense_tracking:expenses')
 
+    # When the form method is get
     else:
+
+        # Generate expense form with selected data from edit button selected
         form = ExpenseForm(instance=expense_to_edit)
+
+        # Render the form with populated data, including drop down values
+        # for expense types
         return render(
             request=request,
             template_name='expense_tracking/edit_expense.html',
@@ -186,18 +231,33 @@ def edit_expense(request, id):
 
 @login_required()
 def delete_expense(request, id):
+    # Function to delete an expense.  User must be logged in to access.
+
+    # Obain expense to delete base on id passed from the form
     expense_to_delete = Expense.objects.get(id=id)
+
+    # Delete the expense
     expense_to_delete.delete()
+
+    # Display alert message stating expense date and name that was deleted.
     messages.success(
         request,
         f'''Expense named {expense_to_delete.name} dated
         {expense_to_delete.expense_date} was successfully deleted!'''
     )
+
+    # Redirect to the table of expenses
     return redirect('expense_tracking:expenses')
 
 
 @login_required()
 def logout_request(request):
+    # Function to log out of the application. User must be logged in to access.
+
     logout(request)
+
+    # Log user out; display alert from log user out signal
     log_user_logout()
+
+    # Redirect to the landing page
     return redirect("catalog:index")
