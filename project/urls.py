662add6f94cba32133be067c4f6e10166fae543d
MKING301 <33675411+MKing301@users.noreply.github.com>
2022-02-23 20:32:45 -0500

    Add api auth path to project urlpatterns

--- project/urls.py
+++ project/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('expense_tracking.urls'))
+    path('', include('expense_tracking.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
