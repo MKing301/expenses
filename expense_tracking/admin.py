462e4c5c4fd5609dbb7452652f2e008741df36c3
MKING301 <33675411+MKing301@users.noreply.github.com>
2024-09-01 08:01:36 -0400

    Register budget model in admin

--- expense_tracking/admin.py
+++ expense_tracking/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import ExpenseType, Expense
+from .models import ExpenseType, Expense, Budget
 
 
 admin.site.register(ExpenseType)
 admin.site.register(Expense)
+admin.site.register(Budget)
